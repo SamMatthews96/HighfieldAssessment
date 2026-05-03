1601ccdb1cf78ebaf5ec4d3c3c1aaefc5490e4b7
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2026-05-03 11:09:54 +0100

    Added parameter validation to courses: will throw an error if there are unexpected parameters, or unexpected types

--- courses.php
+++ courses.php
@@ -1,7 +1,42 @@
 <?php
 
 $string = file_get_contents("courses.json");
-
 $json = json_decode($string, true);
 
-echo json_encode($json);
+$ans = json_encode($json);
+
+// validate _GET
+// check _GET for category, min_price, max_price
+//var_dump($_GET);
+$filters = [];
+foreach ($_GET as $key => $value) {
+    switch ($key) {
+        case 'category':
+            $filters['category'] = $value;
+            break;
+        case 'min_price':
+        case 'max_price':
+            if (is_numeric($value)){
+                $filters[$key] = (float)$value;
+            } else {
+                http_response_code(400);
+                $ans = [
+                    "error" => "Invalid parameter type",
+                    "description" => $key . " must be a number"
+                ];
+                echo json_encode($ans);
+                return;
+            }
+            break;
+        default:
+            http_response_code(400);
+            $ans = [
+                "error" => "Invalid parameter",
+                "description" => $key . " is not a valid parameter. Valid parameters are category, min_price, max_price"
+            ];
+            echo json_encode($ans);
+            return;
+    }
+}
+
+echo $ans;
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 ﻿<html lang="">
 <body>
 <form action="courses.php" method="get">
-
+    <label for="category">Category:</label>
+    <input type="text" name="category"><br>
+    <label for="min_price">Min Price:</label>
+    <input type="number" name="min_price"><br>
+    <label for="max_price">Max Price:</label>
+    <input type="number" name="max_price"><br>
     <button type="submit">Submit</button>
 </form>
 </body>
